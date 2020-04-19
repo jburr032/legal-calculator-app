ac94b8ad33627eb7ca483e8bfbbd4bd7686a595f
Josh Burrill <joshburrill@Joshs-MacBook-Pro.local>
2020-04-19 13:01:10 +0100

    Additional styling on LegalEvent

--- client/src/Components/CourtOrders.js
+++ client/src/Components/CourtOrders.js
@@ -181,7 +181,7 @@ export default class CourtOrdersApp extends Component {
                   </Portal>
                 </Grid.Column>
 
-                <Grid.Column computer={6} table={8} mobile={6}>
+                <Grid.Column computer={6} tablet={8} mobile={6}>
                   {showUserInputs}
                 </Grid.Column>
               </Grid.Row>
