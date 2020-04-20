8e0b0f7bc1c6b1945893539d9245cdc7973663ec
Josh Burrill <joshburrill@Joshs-MacBook-Pro.local>
2020-04-20 17:40:49 +0100

    Fixing incorrect date items for mobile

--- client/src/Components/InvalidDateModal.js
+++ client/src/Components/InvalidDateModal.js
@@ -22,90 +22,87 @@ export default function InvalidDateModal(props) {
   };
 
   return (
-    <Modal open={true} basic size='small'>
-      <Container style={{ marginTop: "250px" }} textAlign='centered'>
-        <Modal.Content scrolling={true}>
-          {" "}
-          <div className='model-invalid-date'>
-            <Modal.Header>
-              <div
-                class='ui large header'
-                style={{ color: "white", marginBottom: "10px" }}>
-                <Icon
-                  name='calendar times outline'
-                  style={{ color: "white" }}
-                />
-                Uh oh!...{datesArr[iterator].type} lands on a weekend or
-                holiday. Please select the date you would prefer:
-              </div>
-            </Modal.Header>
+    <Modal open={true} basic size='small' style={{ paddingTop: "8%" }}>
+      <Modal.Content scrolling={true}>
+        {" "}
+        <div className='model-invalid-date'>
+          <div
+            class='ui large header'
+            style={{ color: "white", marginBottom: "10px" }}>
+            <Icon name='calendar times outline' style={{ color: "white" }} />
+            Uh oh!...
+            {datesArr[iterator].type.length == 1
+              ? "that date"
+              : datesArr[iterator].type}{" "}
+            lands on a weekend or holiday. Please select the date you would
+            prefer:
           </div>
-          <Modal.Actions style={{ textAlign: "center" }}>
-            <Label
-              as='button'
-              color='blue'
-              id={datesArr[iterator].objId}
-              value={[
-                datesArr[iterator].invalidDate[1].calculatedDate,
-                datesArr[iterator].invalidDate[1].diffInDays,
-              ]}
-              onClick={onModalSelection}
-              style={{
-                width: "250px",
-                height: "40px",
-                marginBottom: "10px",
-                fontSize: "14px",
-              }}>
-              {convertDateToString(
-                datesArr[iterator].invalidDate[1].calculatedDate
-              )}
-              {datesArr[iterator].invalidDate[1].holiday ? (
-                <Label.Detail>HOLIDAY</Label.Detail>
-              ) : null}
-            </Label>
+        </div>
+        <Modal.Actions style={{ textAlign: "center" }}>
+          <Label
+            as='button'
+            color='blue'
+            id={datesArr[iterator].objId}
+            value={[
+              datesArr[iterator].invalidDate[1].calculatedDate,
+              datesArr[iterator].invalidDate[1].diffInDays,
+            ]}
+            onClick={onModalSelection}
+            style={{
+              width: "250px",
+              height: "40px",
+              marginBottom: "10px",
+              fontSize: "14px",
+            }}>
+            {convertDateToString(
+              datesArr[iterator].invalidDate[1].calculatedDate
+            )}
+            {datesArr[iterator].invalidDate[1].holiday ? (
+              <Label.Detail>HOLIDAY</Label.Detail>
+            ) : null}
+          </Label>
 
-            <br />
-            <Label
-              as='button'
-              color='blue'
-              id={datesArr[iterator].objId}
-              value={[
-                datesArr[iterator].invalidDate[0].calculatedDate,
-                datesArr[iterator].invalidDate[0].diffInDays,
-              ]}
-              onClick={onModalSelection}
-              style={{
-                width: "250px",
-                height: "40px",
-                marginBottom: "10px",
-                fontSize: "14px",
-              }}>
-              {convertDateToString(
-                datesArr[iterator].invalidDate[0].calculatedDate
-              )}
-            </Label>
-            <Label
-              as='button'
-              color='blue'
-              id={datesArr[iterator].objId}
-              value={[
-                datesArr[iterator].invalidDate[2].calculatedDate,
-                datesArr[iterator].invalidDate[2].diffInDays,
-              ]}
-              onClick={onModalSelection}
-              style={{
-                width: "250px",
-                height: "40px",
-                marginBottom: "10px",
-                fontSize: "14px",
-              }}>
-              {convertDateToString(
-                datesArr[iterator].invalidDate[2].calculatedDate
-              )}
-            </Label>
-          </Modal.Actions>
-        </Modal.Content>
-      </Container>
+          <br />
+          <Label
+            as='button'
+            color='blue'
+            id={datesArr[iterator].objId}
+            value={[
+              datesArr[iterator].invalidDate[0].calculatedDate,
+              datesArr[iterator].invalidDate[0].diffInDays,
+            ]}
+            onClick={onModalSelection}
+            style={{
+              width: "250px",
+              height: "40px",
+              marginBottom: "10px",
+              fontSize: "14px",
+            }}>
+            {convertDateToString(
+              datesArr[iterator].invalidDate[0].calculatedDate
+            )}
+          </Label>
+          <Label
+            as='button'
+            color='blue'
+            id={datesArr[iterator].objId}
+            value={[
+              datesArr[iterator].invalidDate[2].calculatedDate,
+              datesArr[iterator].invalidDate[2].diffInDays,
+            ]}
+            onClick={onModalSelection}
+            style={{
+              width: "250px",
+              height: "40px",
+              marginBottom: "10px",
+              fontSize: "14px",
+            }}>
+            {convertDateToString(
+              datesArr[iterator].invalidDate[2].calculatedDate
+            )}
+          </Label>
+        </Modal.Actions>
+      </Modal.Content>
     </Modal>
   );
 }
