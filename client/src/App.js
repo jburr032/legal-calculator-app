d0a7705cbcb2f2d48a218993500138bd7f1f13f3
Josh Burrill <joshburrill@Joshs-MacBook-Pro.local>
2020-04-21 15:03:45 +0100

    Fixed routing issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./Components/styles.css";
 import CalendarHomePage from "./CalendarHomePage";
-import { withOrientationChange } from "react-device-detect";
 
 const App = (props) => {
-  const { isPortrait } = props;
   return (
     <Router>
-      <Switch>
-        <Route exact to='/uk' component={CalendarHomePage} />
-      </Switch>
-      {isPortrait && <h1>Hello</h1>}
+      <Route exact to='/' component={CalendarHomePage} />
     </Router>
   );
 };
 
-export default withOrientationChange(App);
+export default App;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 //Define routes
-app.use("/", require("./routes/uk-landing"));
+//app.use("/", require("./routes/uk-landing"));
 //app.use("/uk/cpr", require("./routes/cpr"));
 //app.use("/uk/court-order", require("./routes/court-order"));
 
